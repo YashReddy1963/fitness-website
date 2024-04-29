385bc95afacd1a83976e7e9eef0ec9158fd56c40
yash <yash@localhost.localdomain>
2024-04-29 23:07:11 +0530

    added contact form and icon

--- style.js
+++ style.js
@@ -16,13 +16,36 @@ function showSlides(n){
     var i
     var slides = document.getElementsByClassName("slides")
     if(n>slides.length){
+        //if slides is more then lenght, then the first slid will be displayed
         slideIndex = 1
     }
     if(n<1){
+        //if slide is less then 1, then the last slide will be displayed
         slideIndex = slides.length
     }
     for(i=0; i<slides.length; i++){
+        //hidding all the reviews
         slides[i].style.display = "none"
     }
+    //displaying the reviews in fles
     slides[slideIndex - 1].style.display = "flex"
+}
+
+//Reset form
+function resetForm(){
+    document.getElementById("contactForm").reset()
+}
+
+//Submission form
+function submitForm(event){
+    event.preventDefault()
+    console.log("Form Submitted")
+    //to display the submit message
+    document.getElementById("submitMessage").classList.remove("hidden")
+    setTimeout(()=>{
+        //to hide the message after 3 seconds
+        document.getElementById("submitMessage").classList.add("hidden")
+    }, 3000)
+    //resetting the form
+    resetForm()
 }
