f5a01afb2a3db7dfa173ee7c8bebe8ff6b603a76
yash <yash@localhost.localdomain>
2024-05-26 00:28:25 +0530

    Fetched info from api

--- style.js
+++ style.js
@@ -49,3 +49,73 @@ function submitForm(event){
     //resetting the form
     resetForm()
 }
+
+//fitness api
+function fitness(selectElement){
+    var muscle = selectElement.value
+    console.log('Slected element value is: ', muscle)
+    var difficulty = 'intermediate'
+    var type = 'cardio'
+    var name = 'Skating'
+
+    fetch('https://api.api-ninjas.com/v1/exercises?muscle='+muscle+'&difficulty='+difficulty,{
+        method: 'GET',
+        headers:{
+            'X-Api-Key': '5LVeohRiAeSDc0PeRMwY1g==HamqaS7HJICTIrIT',
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        if(!response.ok){
+            throw new Error("Network response was not ok")
+        }
+        return response.json()
+    }).then(data =>{
+        console.log(data)
+        displaySuggestions(data)
+    }).catch(error => {
+        console.error("Error: ", error)
+    })
+}
+
+function displaySuggestions(exercises) {
+    var suggestionsContainer = document.getElementById('suggestions')
+    suggestionsContainer.innerHTML = ''
+
+    exercises.forEach(exercise => {
+        var exerciseElement = document.createElement('div')
+        exerciseElement.className = 'p-2 mb-2 bg-[#1f1f1f] rounded-lg'
+        
+        var nameElement = document.createElement('h3')
+        nameElement.className = 'font-bold text-lg'
+        nameElement.textContent = exercise.name
+
+        var typeElement = document.createElement('p')
+        typeElement.textContent = 'Type: ' + exercise.type
+
+        var muscleElement = document.createElement('p')
+        muscleElement.textContent = 'Muscle: ' + exercise.muscle
+
+        var difficultyElement = document.createElement('p')
+        difficultyElement.textContent = 'Difficulty: ' + exercise.difficulty
+        
+        var equipmentElement = document.createElement('p')
+        equipmentElement.textContent = 'Equipment: ' + exercise.equipment
+
+        var instructionsElement = document.createElement('p')
+        instructionsElement.textContent = 'Instructions: ' + exercise.instructions
+
+        exerciseElement.appendChild(nameElement)
+        exerciseElement.appendChild(typeElement)
+        exerciseElement.appendChild(muscleElement)
+        exerciseElement.appendChild(difficultyElement)
+        exerciseElement.appendChild(instructionsElement)
+
+        suggestionsContainer.appendChild(exerciseElement)
+    })
+}
+
+function resetToDefault(){
+    const selectElement = document.getElementById("body_parts")
+    selectElement.selectedIndex = 0
+    console.clear()
+}
